8049c45657ea8242c16bda9d461b0b6082d5a531
Ivan Koch <ikoch@spiritconsultingllc.com>
2016-11-22 10:12:27 -0800

    added design files

--- ComicWeb/Controllers/ComicBookController.cs
+++ ComicWeb/Controllers/ComicBookController.cs
@@ -10,7 +10,17 @@ namespace ComicWeb.Controllers
     {
         public ActionResult Detail()
         {
-
+            ViewBag.SeriesTitle = "Este es el titulo";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "bla bla bla bla bla bla bla";
+            ViewBag.HtmlEscape = "<p>hola esto es un parrafo html pero q troll vs escapo</p>";
+            ViewBag.HtmlEscapado = "<p>hola esto es un parrafo html con html raw q no escapa</p>";
+            ViewBag.Artists = new string[]
+                {
+                "algo: esto",
+                "esto mas: de esto",
+                "y por ultim: esto"
+                };
             return View();
 
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
