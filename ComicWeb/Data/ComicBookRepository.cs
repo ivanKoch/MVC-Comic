ccf0c901f6d4ca433a95a44715ef4e9797d39a2b
Ivan Koch <ikoch@spiritconsultingllc.com>
2016-11-23 04:20:34 -0800

    eh

--- ComicWeb/Data/ComicBookRepository.cs
+++ ComicWeb/Data/ComicBookRepository.cs
@@ -58,6 +58,12 @@ namespace ComicWeb.Data
                 Favorite = false
             }
          };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
